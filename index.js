d7baa7aba17ad230c640c2b650e60a0e81a863ed
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2024-08-08 21:49:16 +0500

    adding new things
    adding new things

--- index.js
+++ index.js
@@ -57,3 +57,59 @@ var swiper = new Swiper(".review-slider", {
         disableOnInteraction:false,
     }
   });
+
+
+
+//////////////contact form
+function submitForm(){
+    console.log("User Data");
+    JavaScript: void(0);
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const number = document.getElementById('number').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+    console.log(name,email,number,subject);
+  if(name === "" ||email === "" ||number === "" ||subject ==="") {
+    alert('All fields are required');
+  }
+}
+///////////////////////////on focus and blur event
+function onFocus(element) {
+    element.style.backgroundColor = '#fff';
+    element.style.color = '#BF567D';
+}
+
+function onBlur(element) {
+    element.style.backgroundColor = '#eeeeee96';
+}
+
+////////////////////////Home heading
+/////////////////////Mouseover and out event use
+function changeColor() {
+    document.getElementById('heading-clr-change').style.color = '#BF567D';
+    document.getElementById('head-span-chng').style.color = '#fff';
+}
+
+function resetColor() {
+    document.getElementById('heading-clr-change').style.color = '#fff';
+    document.getElementById('head-span-chng').style.color = '#BF567D';
+}
+
+/////////////////////about us section 
+/////////////////////onMouse over
+function swapPic() {
+         document.getElementById("swapPic").src = "assets/images/aboutusImg2.webp";
+}
+function swapPic(state) {
+    const image = document.getElementById('swapPic');
+    if (state === 'hover') {
+        image.src = 'assets/images/aboutusImg2.webp';
+    } else {
+        image.src = 'assets/images/about-us.jpg';
+    }
+}
+
+
+
+
